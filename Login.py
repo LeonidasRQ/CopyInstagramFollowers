c3678bfac74a05e5f1574ac7e0e65e043da433a9
Leonidas <leonidas.ramirez.q@gmail.com>
2020-12-27 18:50:08 -0500

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,7 +1,5 @@
 from instapy import InstaPy
-from instapy import InstaPy
-from instapy.util import smart_run
+
 
 session=InstaPy(username="torombolo546", password="monotiti1").login()
-
-session.follow_by_list()
+session.like_by_tags(["bmw", "mercedes"], amount=5)

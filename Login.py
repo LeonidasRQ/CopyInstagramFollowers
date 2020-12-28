353d67d510adec54ca4f9fdbcf60b4730cb27d33
Leonidas <leonidas.ramirez.q@gmail.com>
2020-12-27 19:57:17 -0500

    FInal
    Final final no va más

--- Login.py
+++ Login.py
@@ -1,5 +1,5 @@
 from instapy import InstaPy
 
-
-session=InstaPy(username="torombolo546", password="monotiti1").login()
-session.like_by_tags(["bmw", "mercedes"], amount=5)
+session=InstaPy(username="torombolo546", password="monotiti1")
+session.login()
+session.follow_user_following('ramlambda',amount=3,randomize=False,interact=False,sleep_delay=600)
